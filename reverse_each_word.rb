ae3a131e35a676e605547f9134b3c64ea0c22e59
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 16:21:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence1)
-  sentence1 = "Hello there, and how are you?"
-    original_array = String.split(" ")
+  string = "Hello there, and how are you?"
+    original_array = string.split(" ")
   return_array = []
   original_array.each do|string|
     return_array << string.reverse
