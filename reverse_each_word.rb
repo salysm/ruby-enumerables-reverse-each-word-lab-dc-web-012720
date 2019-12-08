c229a6382f6b2d76307006c14f2f20bf641e00e1
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 16:22:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,4 +8,12 @@ def reverse_each_word(sentence1)
   return_array.join(" ")
 end
  
-  
+ def reverse_each_word(string)
+   string = ""
+  array = string.split(" ")
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
+  end
+  test_array.join(" ")
+end 
