a7530954edad1cf980e18bfd2c933ae233291250
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 16:29:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,19 +1,17 @@
-def reverse_each_word(sentence1)
-  string = "Hello there, and how are you?"
-    original_array = string.split(" ")
+def reverse_each_word
+ let(:sentence1) { "Hello there, and how are you?"}
+    original_array = sentence1.split(" ")
   return_array = []
   original_array.each do|string|
     return_array << string.reverse
   end
   return_array.join(" ")
 end
- 
- def reverse_each_word_test(string)
-   string = "Hi again, just making sure it's reversed!"
-  array = string.split(" ")
+let(:sentence2){"Hi again, just making sure it's reversed!"}
+  array = sentence2.split(" ")
   test_array = []
-  array.collect do|string| #iterate over the array
-    test_array << string.reverse #reverse each word in the array
+  array.collect do|string| 
+    test_array << string.reverse 
   end
   test_array.join(" ")
 end 
