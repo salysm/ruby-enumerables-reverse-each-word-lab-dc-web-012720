3d50022016e21137c0e003b2a6f4d3db2d98d81f
Sam Smith <smith.samantha.lynn@gmail.com>
2020-01-05 18:04:38 -0500

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,6 @@
-def reverse_each_word
- let(:sentence1) { "Hello there, and how are you?"}
-    original_array = sentence1.split(" ")
-  return_array = []
-  original_array.each do|string|
-    return_array << string.reverse
-  end
-  return_array.join(" ")
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
-let(:sentence2){"Hi again, just making sure it's reversed!"}
-  array = sentence2.split(" ")
-  test_array = []
-  array.collect do|string| 
-    test_array << string.reverse 
-  end
-  test_array.join(" ")
-end 
+
+
+  
